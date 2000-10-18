4dce4d7e695d1f444c448756850fc8a5e6f7750d
lars <lars@da93438f-0fd1-0310-b142-832b9e69d951>
2000-10-18 11:10:35 +0000

    Initial version
    git-svn-id: file:///Library/svn/voice3@3 da93438f-0fd1-0310-b142-832b9e69d951

---
+++ trunk/include/mvip.h
@@ -0,0 +1,81 @@
+/*
+	mvip.h
+
+	Copyright 1995-2000 ibp (uk) Ltd.
+
+	$Id: mvip.h,v 1.1 2000/10/18 11:10:35 lars Exp $
+
+	Author: Lars Immisch <lars@ibp.de>
+*/
+
+#ifndef MVIP_H_
+#define MVIP_H_
+
+#include <bitset>
+
+class MVIP
+{
+public:
+
+	MVIP() {}
+	~MVIP() {}
+	
+	Timeslot allocate()
+	{
+		for (int i = 0; i < timeslots.bitset_size; ++i)
+		{
+			if(!timeslots[i])
+			{
+				timeslots.set(i);
+
+				return Timeslot(i / 32, i % 32);
+			}
+		}
+		throw Exception(__FILE__, __LINE__, "MVIP::allocate()", "MVIP resources exhausted");
+
+		// unreached statement
+		return Timeslot(-1,-1);
+	}
+
+	Timeslot allocate(const Timeslot &a)
+	{
+		if (a.st > 15)
+			throw Exception(__FILE__, __LINE__, "MVIP::allocate()", "invalid MVIP stream");
+
+		int p = a.st * 32 + a.ts;
+
+		if (timeslots[p])
+			throw Exception(__FILE__, __LINE__, "MVIP::allocate()", "MVIP slot in use");
+
+		timeslots.set(p);
+
+		return Timeslot(a);
+	}
+
+	void release(const Timeslot &a)
+	{
+		if (a.st > 15)
+			throw Exception(__FILE__, __LINE__, "MVIP::release()", "invalid MVIP stream");
+
+		int p = a.st * 32 + a.ts;
+
+		if (timeslots[p])
+			throw Exception(__FILE__, __LINE__, "MVIP::release()", "MVIP timeslot not in use");
+
+		timeslots.reset(p);
+	}
+
+	int inUse(const Timeslot &a) const
+	{
+		if (a.st > 15)
+			throw Exception(__FILE__, __LINE__, "MVIP::inUse()", "invalid MVIP stream");
+
+		return timeslots[a.st * 32 + a.ts];
+	}
+	
+protected:
+	
+	std::bitset<512> timeslots;
+};
+
+#endif
