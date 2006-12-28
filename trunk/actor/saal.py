5beb8deb0a7b8b1e131eb7b7c87fd175be81eaa8
lars <lars@da93438f-0fd1-0310-b142-832b9e69d951>
2006-12-28 22:05:54 +0000

    Added streaming daemon for the conference rooms. "Du hast keine Post" is now: "Du hast keine Nachrichten".
    git-svn-id: file:///Library/svn/voice3@241 da93438f-0fd1-0310-b142-832b9e69d951

---
+++ trunk/actor/saal.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import urllib
+import os
+import logging
+import time
+import threading
+import socket
+
+def run(outfile, port):
+    log.debug('Saal %d reader thread started', port - 10000)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while True:
+        d = outfile.read(160)
+        if not d:
+            time.sleep(1.0)
+        # log.debug('sent %d', len(d))
+        s.sendto(d, ('127.0.0.1', port))
+
+def recode(url, port):
+    cmd = 'ffmpeg -f ogg -i - -vc none -f alaw -ar 8000 -ac 1 -'
+    log.debug(cmd)
+    
+    infile, outfile = os.popen2(cmd, 'b')
+
+    t = threading.Thread(target=run, name='Saal%d reader' % (port - 1000),
+                         args=(outfile, port))
+    t.setDaemon(True)
+    t.start()
+    
+    while True:
+        try:
+            stream = urllib.urlopen(url)
+        except:
+            log.error('could not open %s', url, exc_info=1)
+
+        log.debug('opened %s', url)
+
+        while True:
+            # 182 should be about our framerate
+            s = stream.read(182)
+            if not s:
+                log.debug('no data from %s', url)
+                time.sleep(10.0)
+                break
+            infile.write(s)
+
+
+if __name__ == '__main__':
+    
+    logging.basicConfig(format = '%(asctime)s %(levelname)-5s %(message)s',
+                        level=logging.DEBUG)
+    log = logging.getLogger()
+
+    streams = [('http://bytebooster.ulm.ccc.de:8000/saal1.ogm', 10001),
+               ('http://bytebooster.ulm.ccc.de:8000/saal2.ogm', 10002),
+               ('http://bytebooster.ulm.ccc.de:8000/saal3.ogm', 10003),
+               ('http://bytebooster.ulm.ccc.de:8000/saal4.ogm', 10004)]
+               
+    for s in streams[1:]:
+        t = threading.Thread(target=recode, name='Saal%d' % (s[1] - 1000),
+                         args=(s[0], s[1]))
+        t.setDaemon(True)
+        t.start()
+
+    recode(*streams[0])
