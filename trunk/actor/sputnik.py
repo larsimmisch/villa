58073ba34d27868bc78d49f82e236e01ad2559cd
lars <lars@da93438f-0fd1-0310-b142-832b9e69d951>
2007-01-01 15:02:07 +0000

    Fiddling with sputnik data. But I should be reading the binary data instead.
    git-svn-id: file:///Library/svn/voice3@244 da93438f-0fd1-0310-b142-832b9e69d951

--- trunk/actor/sputnik.py
+++ trunk/actor/sputnik.py
@@ -1,10 +1,38 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.4
 
 import urllib
 import re
 import sys
 
-# <observation observer="http://sputnik.congress.ccc.de/tracking-observers/10.254.1.16" id="254002" observed-object="http://sputnik.congress.ccc.de/tracking-users/254002" time="1167491168.0e0" priority="0" min-distance="0.0" max-distance="51.000000e0" direction="[0,0,0]"/>
+import StringIO
+
+obs_data = '<observation observer="http://sputnik.congress.ccc.de/tracking-observers/10.254.1.16" id="254002" observed-object="http://sputnik.congress.ccc.de/tracking-users/254002" time="1167491168.0e0" priority="0" min-distance="0.0" max-distance="51.000000e0" direction="[0,0,0]"/>'
+
+from types import *
+from xml import sax
+
+def parseDirection(dir):
+    return [int(d) for d in dir[1:-1].split(',')]
+
+class Observation(object):
+    def __init__(self, attrs):
+        for t in attrs:
+            if t[0] in ['time', 'min-distance', 'max-distance']:
+                setattr(self, t[0], float(t[1]))
+            elif t[0] == 'priority':
+                setattr(self, t[0], int(t[1]))                
+            elif t[0] == 'direction':
+                setattr(self, t[0], parseDirection(t[1]))                
+            else:
+                setattr(self, *t)
+
+class ObservationParser(sax.ContentHandler):
+    def __init__(self):
+        self.stack = []
+
+    def startElement(self, name, attrs):
+        if name == "observation":
+            self.stack.append(Observation(attrs.items()))
 
 def track(id):
     f = urllib.urlopen('http://sputnik.congress.ccc.de:8000/0')
@@ -18,7 +46,11 @@ def track(id):
         l = f.readline()
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        track(sys.argv[0])
-    else:
-        track('254101')
+
+    parser = ObservationParser()
+    obs = sax.parseString(obs_data, parser)
+
+##     if len(sys.argv) > 1:
+##         track(sys.argv[0])
+##     else:
+##         track('254101')
